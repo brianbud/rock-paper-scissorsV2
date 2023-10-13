98f5a03783dc1ef69a486aa71169115bf1a46af7
Brian Bud <hello@brianbud.com>
2023-10-13 11:01:07 -0400

    pc returns either rock, paper, scissors randomly

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("hello");
+function getComputerChoice() {
+  let choices = ["rock", "paper", "scissors"];
+  let randNum = Math.floor(Math.random() * 3);
+  return choices[randNum];
+}
+
+console.log(getComputerChoice());
