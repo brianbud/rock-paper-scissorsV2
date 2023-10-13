82b96ecf01688474ec00b23b5432dc66bcc1d169
Brian Bud <hello@brianbud.com>
2023-10-13 11:22:12 -0400

    playerSelection to lowercase

--- script.js
+++ script.js
@@ -4,4 +4,16 @@ function getComputerChoice() {
   return choices[randNum];
 }
 
-console.log(getComputerChoice());
+//func with 2 params , playerSelection and Comp selection
+//get playerInput
+// input should be lowercase
+//get pc input
+//return message
+
+function playRound(playerSelection, computerSelection) {
+  return playerSelection.toLowerCase();
+}
+
+const playerSelection = "ROck";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
