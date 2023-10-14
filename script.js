c5a3e229276a0c85e3c444992f44fa3ea1f3f6a4
Brian Bud <hello@brianbud.com>
2023-10-14 12:15:45 -0400

    console score

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   let choices = ["rock", "paper", "scissors"];
   let randNum = Math.floor(Math.random() * 3);
@@ -8,14 +11,18 @@ function playRound(playerSelection, computerSelection) {
   let result = "";
 
   if (playerSelection === "rock" && computerSelection === "paper") {
+    computerScore++;
     result = "You Lose! Paper beats Rock";
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    computerScore++;
     result = "You Lose! Scissors beat Paper";
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    computerScore++;
     result = "You Lose! Rock beats Scissors";
   } else if (playerSelection === computerSelection) {
     result = "Draw";
   } else {
+    playerScore++;
     result = `You Win! ${playerSelection} beats ${computerSelection}`;
   }
 
@@ -23,12 +30,14 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
-  for (let i = 0; i < 6; i++) {
-    let playerSelection = prompt("what do you choose?");
-    playerSelection = playerSelection.toLowerCase();
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt("what do you choose").toLowerCase();
+    let computerSelection = getComputerChoice();
     console.log(playRound(playerSelection, computerSelection));
   }
+
+  console.log(`Your Score: ${playerScore}`);
+  console.log(`Computer Score: ${computerScore}`);
 }
 
-const computerSelection = getComputerChoice();
 game();
